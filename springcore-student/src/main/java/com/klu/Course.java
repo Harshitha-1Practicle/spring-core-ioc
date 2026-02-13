9062fd3bf736635e413244848ea2dc01b10ea7c5
Lavankula Harshitha <harshithalavankula@gmail.com>
2026-02-13 11:56:47 +0530

    Add files via upload

---
+++ springcore-student/src/main/java/com/klu/Course.java
@@ -0,0 +1,28 @@
+
+package com.klu;
+public class Course {
+
+    private String courseName;
+    private int duration;   // duration in months
+
+    // Constructor Injection
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    // Getters
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    // Display format
+    @Override
+    public String toString() {
+        return courseName + " (" + duration + " months)";
+    }
+}
